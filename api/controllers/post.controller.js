4338af427c735d7942f3b177bea4fe205f69601d
kinghennry <keelechiogbu@gmail.com>
2024-11-09 16:24:45 +0100

    add utils folder

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,30 +1,30 @@
 import Post from '../models/post.model.js'
-import { errorHandler } from '../coderecur-api/utils/error.js'
+// import { errorHandler } from '../coderecur-api/utils/error.js'
 
-export const create = async (req, res, next) => {
-  // if (!req.user.isAdmin) {
-  //   return next(errorHandler(403, 'You are not allowed to create a post'))
-  // }
-  if (!req.body.title || !req.body.content) {
-    return next(errorHandler(400, 'Please provide all required fields'))
-  }
-  const slug = req.body.title
-    .split(' ')
-    .join('-')
-    .toLowerCase()
-    .replace(/[^a-zA-Z0-9-]/g, '')
-  const newPost = new Post({
-    ...req.body,
-    slug,
-    userId: req.user.id,
-  })
-  try {
-    const savedPost = await newPost.save()
-    res.status(201).json(savedPost)
-  } catch (error) {
-    next(error)
-  }
-}
+// export const create = async (req, res, next) => {
+//   // if (!req.user.isAdmin) {
+//   //   return next(errorHandler(403, 'You are not allowed to create a post'))
+//   // }
+//   if (!req.body.title || !req.body.content) {
+//     return next(errorHandler(400, 'Please provide all required fields'))
+//   }
+//   const slug = req.body.title
+//     .split(' ')
+//     .join('-')
+//     .toLowerCase()
+//     .replace(/[^a-zA-Z0-9-]/g, '')
+//   const newPost = new Post({
+//     ...req.body,
+//     slug,
+//     userId: req.user.id,
+//   })
+//   try {
+//     const savedPost = await newPost.save()
+//     res.status(201).json(savedPost)
+//   } catch (error) {
+//     next(error)
+//   }
+// }
 
 export const getposts = async (req, res, next) => {
   try {
@@ -71,18 +71,18 @@ export const getposts = async (req, res, next) => {
   }
 }
 
-export const deletepost = async (req, res, next) => {
-  //* already handled in check-auth ❗
-  // if (req.user.id !== req.params.userId) {
-  //   return next(errorHandler(403, 'You are not allowed to delete this post'))
-  // }
-  try {
-    await Post.findByIdAndDelete(req.params.postId)
-    res.status(200).json('The post has been deleted')
-  } catch (error) {
-    next(error)
-  }
-}
+// export const deletepost = async (req, res, next) => {
+//   //* already handled in check-auth ❗
+//   // if (req.user.id !== req.params.userId) {
+//   //   return next(errorHandler(403, 'You are not allowed to delete this post'))
+//   // }
+//   try {
+//     await Post.findByIdAndDelete(req.params.postId)
+//     res.status(200).json('The post has been deleted')
+//   } catch (error) {
+//     next(error)
+//   }
+// }
 
 // export const updatepost = async (req, res, next) => {
 //   if (req.user.id !== req.params.userId) {
